5a0cf532f21d7ae68f18773f1bac7ab5d49e5b30
limabean49 <liammac319@gmail.com>
2025-06-04 13:40:44 +0000

    update main

--- src/gui.py
+++ src/gui.py
@@ -1,4 +1,4 @@
-from tkinter import *
+from customtkinter import *
 import requests
 from PIL import ImageTk, Image
 from io import BytesIO
@@ -7,40 +7,41 @@ class GUI:
     def __init__(self, callback):
         self.callback = callback
 
-        self.main = Tk()
+        self.main = CTk()
         self.main.geometry('1280x720')
         self.main.title("YT Downloader")
+        self.set_appearance_mode("dark")
         self.inputPage()
 
     def inputPage(self):
         for i in self.main.winfo_children():
             i.destroy()
 
-        self.text = Label(self.main, text="Enter a YouTube video link to download:", font=("Helvetica", 20))
+        self.text = CTkLabel(self.main, text="Enter a YouTube video link to download:", font=("Helvetica", 20))
         self.text.pack()
 
-        self.entry = Entry(self.main, width=40, font=("Arial", 14))
+        self.entry = CTkEntry(self.main, width=40, font=("Arial", 14))
         self.entry.pack(pady=20)
 
-        self.submitButton = Button(self.main, text="Submit", font=("Helvetica", 20), command=self.buttonClicked)
+        self.submitButton = CTkButton(self.main, text="Submit", font=("Helvetica", 20), corner_radius=20, command=self.buttonClicked)
         self.submitButton.pack(pady=20)
 
-        self.label = Label(self.main, text="", font=("Helvetica", 20))
+        self.label = CTkLabel(self.main, text="", font=("Helvetica", 20))
         self.label.pack()
 
-        self.image = Label(self.main)
+        self.image = CTkLabel(self.main)
         self.image.pack()
 
-        self.download = Button(self.main, text="Next", font=("Helvetica", 20),  command=self.downloadPage)
+        self.download = CTkButton(self.main, text="Next", font=("Helvetica", 20),  command=self.downloadPage)
 
     def downloadPage(self):
         for i in self.main.winfo_children():
             i.destroy()
         
-        self.back = Button(self.main, text="Back", font=("Helvetica", 20),  command=self.inputPage)
+        self.back = CTkButton(self.main, text="Back", font=("Helvetica", 20),  command=self.inputPage)
         self.back.place(relx=0.1, rely=0.9, anchor=CENTER)
         
-        self.text = Label(self.main, text="Choose settings for download:", font=("Helvetica", 20))
+        self.text = CTkLabel(self.main, text="Choose settings for download:", font=("Helvetica", 20))
         self.text.pack()
 
     def buttonClicked(self):
